1a7dfd15cd1b8cd05dacb1748a8e66430e5bebab
patel25rashmi <75354354+patel25rashmi@users.noreply.github.com>
2022-06-19 23:27:47 +0530

    Add files via upload

---
+++ project - anthropologie-unit 3/footer.js
@@ -0,0 +1,83 @@
+function footer(){
+    let x = 
+    `
+    // <div id="footer">
+        <div id="upper-footer">
+            <div>
+                <h3>Sign Up for Email</h3>
+                <p>Receive early access to new arrivals, sales, exclusive content, events and much more!</p>
+            </div>
+            <div id="mail-button">
+                <h4>Email Address</h4>
+                <input type="text">
+                <button>Submit</button>
+            </div>
+        </div>
+        <div id="para"> <p>By signing up, you will receive Anthropologie offers, promotions and other commercial messages. You are also agreeing to Anthropologie’s Privacy Policy. You may unsubscribe at any time.</p></div>
+        <hr>
+        <div id="middle-footer">
+            <div>
+               <h4>Help</h4> 
+               <p>Track your order</p>
+               <p>Start a return or Exchange(US)</p>
+               <p>Returns and Exchanges</p>
+               <p>Shipping</p>
+               <p>Customer services</p>
+               <p>current Promotions</p>
+               <p>Product Recalls</p>
+            </div>
+            <div>
+                <h4>About Us</h4>
+                <p>Our Story</p>
+                <p>Events</p>
+                <p>A Greater good</p>
+                <p>Diversity and Inclusion</p>
+                <p>Stories</p>
+            </div>
+            <div>
+                <h4>Services</h4>
+                <p>AnthroPerks</p>
+                <p>Gift Cards</p>
+                <p>Trade Membership</p>
+                <p>Furniture: Guides & Services</p>
+                <p>Store Pickup & Collection Points</p>
+                <p>Klarna</p>
+            </div>
+            <div>
+                <h4>Connect</h4>
+                <p>Contact Us</p>
+                <p>Stay Connected</p>
+                <p>Carrers</p>
+                <p>Styling Services</p>
+                <p>Request A Catalog</p>
+            </div>
+            <div>
+                <p><a href="#">Store locator</a></p>
+                <p> <p><a href="#">Get Email</a></p></p>
+            </div>       
+        </div>
+        <div id="logo-footer">
+            <div id="logo">
+                <div><img src="https://images.ctfassets.net/5de70he6op10/4gWq1G4qLLgmqlNeHfzV2T/55298745127c36c5876f4013434c0c37/Download_on_the_App_Store_Badge_US-UK_RGB_wht_092917_Copy_2.svg" alt="appStore"></div>
+                <div><i class="fa-brands fa-pinterest" class="logo-img"></i></div>
+                <div><i class="fa-brands fa-instagram" class="logo-img"></i></div>
+                <div><i class="fa-brands fa-facebook-square" class="logo-img"></i></div>
+                <div><i class="fa-brands fa-twitter" class="logo-img"></i></div>
+            </div>
+            <div><img src="https://images.ctfassets.net/5de70he6op10/2Lh06CXT4AzVnj4LxvGsQD/a424e10a3b8c9e2e270dc22505bfbfa6/022421_SiteFooter_SMS_SIgnUp.jpg" alt="footer-img"></div>
+        </div>
+
+        <div id="bottom-footer">
+            <div>Us</div>
+            <div>France</div>
+            <div>Germany</div>
+            <div>Italy</div>
+            <div>Spain</div>
+            <div>Uk</div>
+        </div>
+    // </div>
+    `
+
+return x;
+}
+export default footer;
---
+++ project - anthropologie-unit 3/navbar.js
@@ -0,0 +1,49 @@
+let navbar = () => {
+    return `
+    <div id="nav1">
+    <p>this weekend only</p> 
+    <p>EXTRA 40% OFF ALL SALE ITEMS!</p>
+    <a href="#">shop</a>
+    <a href="#">all sales final – see details</a>
+</div>
+<div id="nav2">
+    <div><span><i class="fa-thin fa-circle-microphone"></i></span><p>English</p></div>
+    <div><span><i class="fa-light fa-user"></i></span> <p>Sign In</p></div>
+</div>
+<div id="nav3">
+    <div id="page">
+        <div>ANTHROPOGIE</div>
+        <div>anthro living home</div>
+    </div>
+    <div id="search-cart">
+        <div>
+            <form class="example" action="action_page.php">
+                <input type="text" placeholder="search ANTHROPOGIE" name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+              </form>
+        </div>
+        <div id="cart">
+            <button><i class="fal fa-shopping-bag"></i></button>
+        </div>
+    </div>
+</div>
+<div id="nav4">
+    <div>New!</div>
+    <div>Dresses</div>
+    <div>Clothing</div>
+    <div>Shoes</div>
+    <div>Accessories</div>
+    <div>Gifts & Candles</div>
+    <div>Home & Furniture</div>
+    <div>Beauty & Wellness</div>
+    <div>Garden & Outdoor</div>
+    <div>Wedding</div>
+    <div>Sale</div>
+</div>
+<div id="nav5">
+    <div><img src="https://images.ctfassets.net/5de70he6op10/6jLjT1AyPznRcro3PG6wcj/fb1b201470d651e05c895313ffabdbec/061722_40offSOS_HPG_BBB_LS.jpg?w=1302&q=80&fm=webp" alt="nav5-img1"></div>
+    <div><img src="https://images.ctfassets.net/5de70he6op10/6vLnpJH9VTKImoPrvc41rm/3eea1c0dc91b3ba7e294667779b7037f/061722_DressFlashSale_HPG_Banner_1.jpg?w=1302&q=80&fm=webp" alt="nav5-img2"></div>
+</div>
+    `;
+}
+export default navbar();
